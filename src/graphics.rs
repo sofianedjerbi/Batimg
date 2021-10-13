3c3a79caa956639a46a419e736c0a03c217166d8
kugge <sofiane.djerbi38@gmail.com>
2021-10-13 18:51:42 +0200

    Working API (Resize effect)

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,25 +1,60 @@
-use image::Reader;
+/// graphics.rs - Load images and generate ascii data
+use image::imageops::FilterType;
+use image::imageops::resize;
+use image::ImageError;
+use image::io::Reader;
+use image::RgbImage;
 
-mod graphics {
-    /// Print with colors (r, g, b) on the foreground
-    /// Works on true colors terminal
-    #[macro_export]
-    macro_rules! printcf {
-        ($t: expr, $r: expr, $g: expr, $b: expr) => {
-            println!("\x1b[38;2;{};{};{}m{}", $r, $g, $b, $t);
-        }
+/// Print with colors (r, g, b) on the foreground
+#[macro_export]
+macro_rules! printcf {
+    ($t: expr, $r: expr, $g: expr, $b: expr) => {
+        print!("\x1b[38;2;{};{};{}m{}", $r, $g, $b, $t);
     }
-    /// Print with colors (r, g, b) on the background
-    /// Works on true colors terminall
-    #[macro_export]
-    macro_rules! printcb {
-        ($t: expr, $r: expr, $g: expr, $b: expr) => {
-            println!("\x1b[48;2;{};{};{}m{}", $r, $g, $b, $t);
-        }
+}
+/// Print with colors (r, g, b) on the background
+#[macro_export]
+macro_rules! printcb {
+    ($t: expr, $r: expr, $g: expr, $b: expr) => {
+        print!("\x1b[48;2;{};{};{}m{}", $r, $g, $b, $t);
     }
+}
 
-    fn load_image(path: str) -> Reader {
-        let image: Reader = Reader::open("path/to/image.png")?.decode()?;
-        return image;
+/// Print with colors (r, g, b) on both background and foreground
+#[macro_export]
+macro_rules! printca {
+     ($t: expr, $r: expr, $g: expr, $b: expr) => {
+        print!("\x1b[48;2;{r};{g};{b}m\x1b[38;2;{r};{g};{b}m{t}",
+            r=$r, g=$g, b=$b, t=$t);
+    }
+}
+
+/// Print a square of a single (r, g, b) color
+#[macro_export]
+macro_rules! printc {
+     ($r: expr, $g: expr, $b: expr) => {
+        printca!("X", $r, $g, $b)
+    }
+}
+
+// Load an image
+pub fn load_image(path: &str) -> Result<RgbImage, ImageError> {
+    let image = Reader::open(path)?.decode()?;
+    return Ok(image.to_rgb8());
+}
+
+// Resize image
+pub fn resize_image(img: &RgbImage, w: u32, h: u32) -> RgbImage {
+    return resize(img, w, h, FilterType::Nearest);
+}
+
+// Show image
+pub fn print_image(image: RgbImage){
+    for i in 0..image.height() {
+        for j in 0..image.width() {
+            let px = image.get_pixel(j, i);
+            printc!((*px)[0], (*px)[1], (*px)[2]);
+        }
+        print!("\n");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 mod graphics;
 
 fn main() {
-    printcb!("Hello, world!", 0, 255, 0);
+    let raw_img = graphics::load_image("diams.png");
+    let img = raw_img.unwrap();
+    let w = img.width();
+    let h = img.height();
+    let img = graphics::resize_image(&img, w/2, h/4);
+    graphics::print_image(img);
 }

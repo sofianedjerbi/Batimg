b827aff6d4ea9b8383b73e9e6b0eb5f636a5fe23
Kugge <sofiane.djerbi38@gmail.com>
2021-11-27 18:10:57 +0100

    Added video playing support

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,4 +1,9 @@
 /// graphics.rs - Load images and generate ascii data
+use std::process::Command;
+use std::time::{Duration, Instant};
+use std::thread::sleep;
+use std::str;
+
 use image::imageops::FilterType;
 use image::imageops::resize;
 use image::ImageError;
@@ -49,7 +54,7 @@ pub fn resize_image(img: &RgbImage, w: u32, h: u32) -> RgbImage {
 }
 
 // Show image
-pub fn print_image(image: RgbImage){
+pub fn print_image(image: RgbImage) {
     for i in 0..image.height() {
         for j in 0..image.width() {
             let px = image.get_pixel(j, i);
@@ -58,3 +63,102 @@ pub fn print_image(image: RgbImage){
         print!("\x1b[0m\n");
     }
 }
+
+// Process and print an image
+pub fn process_image(file: &str, height: u32){
+    let raw_img = load_image(file);
+    let img = match raw_img {
+        Ok(pic) => pic,
+        Err(_err) => {
+            eprintln!("{}: Unknown file format.", file);
+            std::process::exit(4);
+        },
+    };
+    let w = img.width();
+    let h = img.height();
+    let img = resize_image(&img, w*height/h, height/2);
+    print_image(img);
+}
+
+pub fn process_video(file: &str, height: u32){
+    /*** PREPROCESSING ***/
+    // Setting default incrementation (ideal)
+    let mut incr: f64 = 1.;
+    // Current frame
+    let mut frame: f64 = 0.;
+    // Getting total number of frames
+    let raw_frames = Command::new("ffprobe")
+        .arg("-v")
+        .arg("error")
+        .arg("-select_streams")
+        .arg("v:0")
+        .arg("-count_packets")
+        .arg("-show_entries")
+        .arg("stream=nb_read_packets")
+        .arg("-of")
+        .arg("csv=p=0")
+        .arg(file)
+        .output()
+        .expect("Failed to execute FFprobe process.");
+    let total_frames = str::from_utf8(&raw_frames.stdout)
+        .unwrap()
+        .replace("\n", "")
+        .parse::<u32>()
+        .unwrap() as f64;
+    // Getting fps
+    let ffprobe_fps = Command::new("ffprobe")
+        .arg("-v")
+        .arg("error")
+        .arg("-select_streams")
+        .arg("v")
+        .arg("-show_entries")
+        .arg("stream=r_frame_rate")
+        .arg("-of")
+        .arg("default=noprint_wrappers=1:nokey=1")
+        .arg(file)
+        .output()
+        .expect("Failed to execute FFprobe process.");
+    let str_fps = str::from_utf8(&ffprobe_fps.stdout)
+        .unwrap()
+        .replace("\n", "");
+    let raw_fps: Vec<&str> = str_fps.split("/").collect();
+    // dividende
+    let fps1 = raw_fps[0].parse::<f64>().unwrap();
+    // divisor
+    let fps2 = raw_fps[1].parse::<f64>().unwrap();
+    // Second per frame
+    let spf = fps2/fps1;
+    // Duration per frame
+    let dpf = Duration::from_secs_f64(spf);
+    // Hide cursor
+    print!("\x1b[?25l");
+    Command::new("clear").status().unwrap(); // Clear term
+    /*** PROCESSING ***/
+    while frame < total_frames {
+        let now = Instant::now();
+        // Get frame
+        print!("\x1b[2H");
+        Command::new("ffmpeg")
+            .arg("-ss")
+            .arg((spf*frame).to_string())
+            .arg("-y")
+            .arg("-i")
+            .arg(file)
+            .arg("-vf")
+            .arg("select=eq(n\\,1)")
+            .arg("advideo.tmp.bmp")
+            .output()
+            .expect("Failed to execute FFmpeg process.");
+        // Print frame
+        process_image(&"advideo.tmp.bmp", height);
+        // Check fps, and sleep if needed
+        match dpf.checked_sub(now.elapsed()) {
+            Some(duration) => sleep(duration),
+            None => incr += 1. // Incr frameskip if cant keep up
+        };
+        frame += incr;
+    }
+    Command::new("clear").status().unwrap(); // Clear term
+    print!("\x1b[?25h");
+}
+

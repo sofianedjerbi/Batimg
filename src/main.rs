ac5062f2e581750fe36ace30eea5b4c1ba6b353f
Kugge <sofiane.djerbi38@gmail.com>
2021-11-27 13:09:20 +0100

    Better error handling Deleted useless comments Fit image into terminal

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use terminal_size::{Width, Height, terminal_size};
 use clap::{App, Arg};
+use std::path::Path;
+use std::cmp::min;
 
 mod graphics;
 
@@ -7,72 +9,70 @@ mod graphics;
 fn main() {
     // Load cli config
     let matches = App::new("Ascii DRIP")
-        .version("0.0")
+        .version("0.1")
         .author("Sofiane D. <@Kugge>")
         .about("Graphic content on your tty")
-        //.arg(Arg::new("picture")
-        //    .short('p')
-        //    .long("picture")
-        //    .about("Show a picture"))
-        .arg(Arg::new("size")
+        .arg(Arg::new("SIZE")
             .short('s')
             .long("size")
             .value_name("u32")
             .about("Canvas size")
             .takes_value(true))
         .arg(Arg::new("FILE")
-            .about("Sets the input file to use")
+            .about("Path to the media")
+            .value_name("FILE")
             .required(true)
+            .takes_value(true)
             .index(1))
         .get_matches();
 
     // Variables to populate
     let file: &str;
-    //let width: u32;
     let height: u32;
-    //let is_video: bool;
-    //let is_picture: bool;
+    let is_video: bool;
+    let is_picture: bool;
 
     // GET CANVAS SIZE
     let size = terminal_size(); // Request term size
     if let Some(h) = matches.value_of("size") { // In options
         height = h.parse().unwrap();
     }
-    else if let Some((Width(_w), Height(h))) = size { // In terminal
+    else if let Some((Width(w), Height(h))) = size { // In terminal
         //width=w as u32;
-        height=h as u32;
+        height=min(h, w/2) as u32;
     }
     else { // Cannot get terminal size
-        println!("Unable to get canvas size, please use <--size WxH>.");
-        return
+        eprintln!("Unable to get canvas size, please use <--size> option.");
+        std::process::exit(3);
     }
 
     // GET INPUT FILE
     if let Some(f) = matches.value_of("FILE") {
         file = &f;
     } else {
-        println!("No input files.");
-        return
+        eprintln!("No media specified.");
+        std::process::exit(1);
     }
 
     //is_video = matches.is_present("video");
     //is_picture = matches.is_present("picture");
 
-    /*
-    if is_picture && is_video {
-        println!("error: Incompatible flags --video and --picture");
-        return
-    }
-    else if !is_picture && !is_video {
-        println!("error: Please specify either --video or --picture");
-        return
+    if !Path::new(file).exists() {
+        eprintln!("{}: No such media.", file);
+        std::process::exit(1);
     }
 
     // PROCESS PICTURE
-    if is_picture {
-    */
+    //if is_picture {
     let raw_img = graphics::load_image(file);
-    let img = raw_img.unwrap();
+    let img = match raw_img {
+        Ok(pic) => pic,
+        Err(err) => {
+            eprintln!("There was an error reading media {}", file);
+            eprintln!("Error: {:?}", err);
+            std::process::exit(4);
+        },
+    };
     let w = img.width();
     let h = img.height();
     let img = graphics::resize_image(&img, w*height/h, height/2);
